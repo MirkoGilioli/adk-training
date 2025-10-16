f712725f56d989fa7ca36d1b639dc9dc2009dde8
v-mirko.gilioli <mirko.gilioli@datatonic.com>
2025-10-16 14:56:56 +0200

    Added a new use case where we build a multi agent system that recommends trips and itineraries. It stores the list of itineraries in Tool Context memory

---
+++ 03_multi_agent/multi_travel_agent/attractions_planner/agent.py
@@ -0,0 +1,35 @@
+from google.adk.agents.llm_agent import Agent
+from google.adk.tools.tool_context import ToolContext
+from typing import List
+
+def save_attractions_to_state(
+    tool_context: ToolContext,
+    attractions: List[str]
+) -> dict[str, str]:
+    """
+    Saves the list of attractions to the state["attractions"]
+
+        Args:
+          attractions[str]: a list of strings to add to the list of attractions
+        
+        Returns:
+          None
+    """
+    existing_attractions = tool_context.state.get("attractions", [])
+    tool_context.state["attractions"] = existing_attractions + attractions
+    return {"status": "success"}
+    
+
+attractions_planner = Agent(
+    model='gemini-2.5-flash',
+    name='attractions_planner',
+    description="Build a list of attractions for a given destination.",
+    instruction="""
+        - Build a list of attractions for a given destination by the user
+        - When the user reply with a given list of attractions, use your tools to save their selected attractions
+        - If User ask about what attractions has chosen, provide the list of {{ "attractions" }}
+        """,
+    tools=[
+        save_attractions_to_state
+    ]
+)
